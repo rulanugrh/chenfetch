1f25bede27814e7a07c91d7faaaaae8d00c292d1
Pradana <nextarituenak01@gmail.com>
2024-03-02 15:36:54 +0000

    chore: term

--- janus/main.go
+++ janus/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"os/exec"
 
 	"github.com/gilliek/go-xterm256/xterm256"
 	"github.com/jedib0t/go-pretty/v6/table"
@@ -18,34 +18,39 @@ func main() {
 	for _, cp := range cpus {
 		cpuName = cp.ModelName
 	}
-	
-	data, _ := host.Info()
-  var infoOS string
-  if data.OS == "windows" {
-    infoOS = "  " +xterm256.Sprintf(xterm256.Yellow, "  ")+ xterm256.Sprintf(xterm256.Cyan, strings.Title("windows"))
-  } else{
-    infoOS = "  "+xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.Cyan, strings.Title(data.OS))
-  }
 
-	infoKernel := "  "+xterm256.Sprintf(xterm256.Yellow, "󰀄 ") + xterm256.Sprintf(xterm256.Cyan, strings.Title(data.Hostname))
-	infoPlatform := "  " + "🦪 " + xterm256.Sprintf(xterm256.Cyan, strings.Title(data.Platform))
-	infoCPU := "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.Cyan, strings.Title(cpuName))
+	data, _ := host.Info()
+
+	var infoOS string
+	var shell string
+	if data.OS == "windows" {
+		infoOS = "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.Cyan, data.OS)
+		ex, _ := exec.Command("$Env:POWERLINE_COMMAND").Output()
+		shell = "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.Cyan, string(ex))
+	} else {
+		infoOS = "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.Cyan, data.OS)
+		ex, _ := exec.Command("/bin/sh", "-c", "echo $TERM").Output()
+		shell = "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.Cyan, string(ex))
+	}
+
+	infoKernel := "  " + xterm256.Sprintf(xterm256.Yellow, "󰀄 ") + xterm256.Sprintf(xterm256.Cyan, data.Hostname)
+	infoCPU := "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.Cyan, cpuName)
 
 	t := table.NewWriter()
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.LightGray, "")})
-	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, "          ,MMM8&&&. "), xterm256.Sprintf(xterm256.Cyan, "┌─────────────")+xterm256.Sprintf(xterm256.Red, " Hardware Information ")+ xterm256.Sprintf(xterm256.Cyan, "────────────┐") })
+	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, "          ,MMM8&&&. "), xterm256.Sprintf(xterm256.Cyan, "┌─────────────") + xterm256.Sprintf(xterm256.Red, " Hardware Information ") + xterm256.Sprintf(xterm256.Cyan, "────────────┐")})
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, "     _...MMMMM88&&&&..._ ")})
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, "  .::'''MMMMM88&&&&&&'''::. "), infoOS})
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, " ::     MMMMM88&&&&&&     :: "), infoKernel})
-	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, " '::....MMMMM88&&&&&&....::' "), infoPlatform})
+	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, " '::....MMMMM88&&&&&&....::' "), shell})
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, "    `''''MMMMM88&&&&''''` "), infoCPU})
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, `    jgs   'MMM8&&&' `)})
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, ``), xterm256.Sprintf(xterm256.Cyan, "└───────────────────────────────────────────────┘")})
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, ` `)})
 
-  t.SetStyle(table.StyleLight)
-  t.Style().Title.Align = text.AlignCenter
-  t.SetTitle(xterm256.Sprintf(xterm256.Cyan, "chenfetch"))
+	t.SetStyle(table.StyleLight)
+	t.Style().Title.Align = text.AlignCenter
+	t.SetTitle(xterm256.Sprintf(xterm256.Cyan, "chenfetch"))
 
 	fmt.Println(t.Render())
 }

caa44ace29656d7a4665c3cf49dab5930dc636e6
rulanugrh <nextarituenak01@gmail.com>
2024-03-03 20:03:53 +0700

    chore: update janus theme:v2

--- janus/main.go
+++ janus/main.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/gilliek/go-xterm256/xterm256"
 	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/net"
@@ -35,25 +34,26 @@ func main() {
 	}
 
 	data, _ := host.Info()
-	host := fmt.Sprintf("%s@%s", strings.ToLower(os.Getenv("USERNAME")), data.Hostname)
+	// host := fmt.Sprintf("%s@%s", strings.ToLower(os.Getenv("USERNAME")), data.Hostname)
 
 	var infoOS string
 	var shell string
 	if data.OS == "windows" {
-		infoOS = "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.DarkCyan, data.OS)
+		infoOS = "  " + xterm256.Sprintf(xterm256.DarkCyan, "  ") + xterm256.Sprintf(xterm256.DarkCyan, data.OS)
 		ex := os.Getenv("POWERLINE_COMMAND")
-		shell = "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.DarkCyan, string(ex))
+		shell = "  " + xterm256.Sprintf(xterm256.DarkCyan, "  ") + xterm256.Sprintf(xterm256.DarkCyan, string(ex))
 	} else {
-		infoOS = "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.DarkCyan, data.OS)
+		infoOS = "  " + xterm256.Sprintf(xterm256.DarkCyan, "  ") + xterm256.Sprintf(xterm256.DarkCyan, data.OS)
 		ex, _ := exec.Command("/bin/sh", "-c", "echo $TERM").Output()
-		shell = "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.DarkCyan, string(ex))
+		shell = "  " + xterm256.Sprintf(xterm256.DarkCyan, "  ") + xterm256.Sprintf(xterm256.DarkCyan, string(ex))
 	}
 
-	infoCPU := "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.DarkCyan, cpuName)
-	infoIP := "  " + xterm256.Sprintf(xterm256.Yellow, "  ") + xterm256.Sprintf(xterm256.DarkCyan, ips)
+	infoCPU := "  " + xterm256.Sprintf(xterm256.DarkCyan, "  ") + xterm256.Sprintf(xterm256.DarkCyan, cpuName)
+	infoIP := "  " + xterm256.Sprintf(xterm256.DarkCyan, "  ") + xterm256.Sprintf(xterm256.DarkCyan, ips)
 
 	t := table.NewWriter()
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.LightGray, "")})
+  t.AppendRow(table.Row{xterm256.Sprintf(xterm256.LightGray, "")})
 
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, "          ,MMM8&&&. "), xterm256.Sprintf(xterm256.DarkCyan, "┌──────────────") + xterm256.Sprintf(xterm256.Red, " ハードウェア情報 ") + xterm256.Sprintf(xterm256.DarkCyan, "─────────────┐")})
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, "     _...MMMMM88&&&&..._ "), ""})
@@ -64,10 +64,23 @@ func main() {
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, `    jgs   'MMM8&&&' `)})
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, ``), xterm256.Sprintf(xterm256.DarkCyan, "└─────────────────────────────────────────────┘")})
 	t.AppendRow(table.Row{xterm256.Sprintf(xterm256.Yellow, ` `)})
+  t.AppendRow(table.Row{xterm256.Sprintf(xterm256.LightGray, "")})
 
-	t.SetStyle(table.StyleLight)
-	t.Style().Title.Align = text.AlignCenter
-	t.SetTitle(xterm256.Sprintf(xterm256.Cyan, host))
+	t.SetStyle(table.Style{
+    Name: "newStyle",
+    Box: table.BoxStyle{
+      BottomLeft: "",
+    },
+    Options: table.Options{
+      DrawBorder: false,
+      SeparateColumns: false,
+      SeparateFooter: false,
+      SeparateHeader: false,
+      SeparateRows: false,
+    },
+  })
+	// t.Style().Title.Align = text.AlignCenter
+	// t.SetTitle(xterm256.Sprintf(xterm256.Cyan, host))
 
 	fmt.Println(t.Render())
 }
